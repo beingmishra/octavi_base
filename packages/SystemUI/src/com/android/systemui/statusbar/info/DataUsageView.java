i-o-n/frameworks_base

QSHeaderDataUsage: show Wi-Fi usage if device is connected to Wi-Fi
Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.provider.Settings;
 import android.view.View;
 
+import com.android.internal.util.ion.IonUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -58,8 +59,12 @@ public class DataUsageView extends TextView {
         mobileDataController.setSubscriptionId(
             SubscriptionManager.getDefaultDataSubscriptionId());
         final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
-                mobileDataController.getDailyDataUsageInfo()
-                : mobileDataController.getDataUsageInfo();
+                (IonUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (IonUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
 
         formatedinfo = formatDataUsage(info.usageLevel);
     }

beingmishra/octavi_base

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.provider.Settings;
 import android.view.View;
 
+import com.android.internal.util.derp.DerpUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -59,10 +60,13 @@ public class DataUsageView extends TextView {
         mobileDataController.setSubscriptionId(
             SubscriptionManager.getDefaultDataSubscriptionId());
         final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
-                mobileDataController.getDailyDataUsageInfo()
-                : mobileDataController.getDataUsageInfo();
-
-        formatedinfo = formatDataUsage(info.usageLevel);
+                (DerpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (DerpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
+        formatedinfo = formatDataUsage(info.usageLevel) + " ";
     }
 
     public int isDataUsageEnabled() {